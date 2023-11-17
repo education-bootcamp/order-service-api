e62cb48533d95571b4d595873c542711f440c83f
education-bootcamp <education.seekerscloud@gmail.com>
2023-11-17 21:08:16 +0530

    3

--- src/main/java/com/devstack/orderserviceapi/api/OrderController.java
+++ src/main/java/com/devstack/orderserviceapi/api/OrderController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/orders")
 public class OrderController {
@@ -32,13 +34,10 @@ public class OrderController {
     }
 
     @GetMapping("/get-by-customer-id/{id}")
-    public ResponseEntity<StandardResponseEntity> findOrdersByCustomer(
+    public List<OrderDto> findOrdersByCustomer(
             @PathVariable Long id
     ){
-        return new ResponseEntity<>(
-                new StandardResponseEntity(201,"Orders by customer!", orderService.loadOrdersByCustomer(id)),
-                HttpStatus.CREATED
-        );
+        return orderService.loadOrdersByCustomer(id);
     }
 
 }
